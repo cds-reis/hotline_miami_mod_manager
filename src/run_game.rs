8dae1ef1af2b0dd578c561ad4322ce5b835985ed
cardosoOReis <rafaelcdsreis@gmail.com>
2024-01-26 20:54:11 -0300

    build command

--- src/run_game.rs
+++ src/run_game.rs
@@ -1,17 +1,21 @@
 use std::process::{exit, Command};
 
 pub fn run_hotline_miami_2() -> ! {
-    let mut command = Command::new("cmd");
-    let program = command.args(["/C", "start", "", HOTLINE_MIAMI_2_STEAM_URL]);
-    let output = program.output();
+    let output = build_command().output();
     match output {
         Ok(_) => exit(0),
         Err(err) => {
             eprintln!("Something went wrong while opening the game.");
             eprintln!("Error: {:?}", err);
             exit(123)
-        },
+        }
     }
+}
+
+fn build_command() -> Command {
+    let mut command = Command::new("cmd");
+    command.args(["/C", "start", "", HOTLINE_MIAMI_2_STEAM_URL]);
+    command
 }
 
 const HOTLINE_MIAMI_2_STEAM_URL: &str = r"steam://rungameid/274170";
