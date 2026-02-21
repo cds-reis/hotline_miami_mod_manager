761e9168ed546f6788d52ce3ff9c7a61ac8a0786
Rafael Reis <rafaelcdsreis@gmail.com>
2026-02-20 21:03:19 -0300

    added option to run hotline miami 2 on Linux

--- src/run_game.rs
+++ src/run_game.rs
@@ -3,11 +3,10 @@ use std::{io, process::Command};
 use thiserror::Error;
 
 pub fn run_hotline_miami_2() -> Result<(), RunHotlineMiamiError> {
-    let output = build_command().output();
-    match output {
-        Ok(_) => Ok(()),
-        Err(err) => Err(RunHotlineMiamiError::from(err)),
-    }
+    build_command()
+        .output()
+        .map_err(RunHotlineMiamiError::from)
+        .map(|_| ())
 }
 
 #[derive(Error, Debug)]
@@ -17,9 +16,15 @@ pub enum RunHotlineMiamiError {
 }
 
 fn build_command() -> Command {
-    let mut command = Command::new("cmd");
-    command.args(["/C", "start", "", HOTLINE_MIAMI_2_STEAM_URL]);
-    command
+    if cfg!(target_os = "windows") {
+        let mut command = Command::new("cmd");
+        command.args(["/C", "start", "", HOTLINE_MIAMI_2_STEAM_URL]);
+        command
+    } else {
+        let mut command = Command::new("xdg-open");
+        command.arg(HOTLINE_MIAMI_2_STEAM_URL);
+        command
+    }
 }
 
-const HOTLINE_MIAMI_2_STEAM_URL: &str = r"steam://rungameid/274170";
+const HOTLINE_MIAMI_2_STEAM_URL: &str = "steam://rungameid/274170";
